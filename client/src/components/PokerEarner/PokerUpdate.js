02b685f4c530587eb11d88d08329f2bfa571694f
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-07-17 18:48:44 -0400

    Oops

--- client/src/components/PokerEarner/PokerUpdate.js
+++ client/src/components/PokerEarner/PokerUpdate.js
@@ -4,19 +4,21 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Form, Button, Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// Component to update poker game information
 const PokerUpdate = () => {
+    // Use the useNavigate hook to handle navigation
     const navigate = useNavigate();
-    const { id } = useParams(); // Fetch the id from the URL
 
-    // Initialize state variables for form fields and error handling
+    // Get the "id" parameter from the URL
+    const { id } = useParams();
+
+    // Set up state variables
     const [result, setResult] = useState("Win");
     const [amount, setAmount] = useState("");
     const [location, setLocation] = useState("");
     const [date, setDate] = useState("");
     const [error, setError] = useState("");
 
-    // Function to format date strings
+    // Helper function to format the date
     const formatDate = (dateString) => {
         let serverDate = new Date(dateString);
         let adjustedDate = new Date(
@@ -26,12 +28,11 @@ const PokerUpdate = () => {
         return dateToShow;
     };
 
-    // UseEffect to fetch game data from the server on component mount
+    // Fetch the poker game data when the component mounts
     useEffect(() => {
         axios
-            .get("http://localhost:8000/api/poker/" + id)
+            .get(`http://localhost:8000/api/poker/${id}`)
             .then((res) => {
-                console.log(res.data);
                 setResult(res.data.result);
                 setAmount(res.data.amount);
                 setLocation(res.data.location);
@@ -43,18 +44,20 @@ const PokerUpdate = () => {
             });
     }, [id]);
 
-    // Function to handle form submission, validates input, makes PUT request to server to update game data
+    // Function to handle the form submission
     const updatePokerGame = (e) => {
         e.preventDefault();
         setError(""); // Reset error state
 
+        // Validate the form fields
         if (!amount || !location || !date) {
-            setError("All fields are required"); // Set error message if any field is empty
+            setError("All fields are required");
             return;
         }
 
+        // Send a PUT request to update the poker game
         axios
-            .put("http://localhost:8000/api/poker/" + id, {
+            .put(`http://localhost:8000/api/poker/${id}`, {
                 result,
                 amount,
                 location,
@@ -62,7 +65,7 @@ const PokerUpdate = () => {
             })
             .then((res) => {
                 console.log(res);
-                navigate(-1); // After successful submission, navigate back to previous page
+                navigate(-1); // Navigate back to the previous page
             })
             .catch((err) => {
                 console.log(err);
@@ -70,34 +73,63 @@ const PokerUpdate = () => {
             });
     };
 
-    // Return form with error alert (if any) and form fields for result, amount, location and date
     return (
         <div className="w-75 mx-auto bg-dark text-light rounded m-5 pb-5">
             <h2 className="fs-2 my-5 pt-5 text-center">Update Poker Game</h2>
             <hr />
             <Form onSubmit={updatePokerGame} className="w-50 mx-auto fs-5">
                 {error && <Alert variant="danger">{error}</Alert>}
-                {/* Form group for result (win/loss) radio buttons */}
                 <Form.Group className="mt-5">
                     <div>
                         <Form.Label className="me-3">Result:</Form.Label>
-                        {/*... Form.Check code ...*/}
+                        <Form.Check
+                            inline
+                            type="radio"
+                            id="win"
+                            label="Win"
+                            name="result"
+                            value="Win"
+                            checked={result === "Win"}
+                            onChange={() => setResult("Win")}
+                        />
+                        <Form.Check
+                            inline
+                            type="radio"
+                            id="loss"
+                            label="Loss"
+                            name="result"
+                            value="Loss"
+                            checked={result === "Loss"}
+                            onChange={() => setResult("Loss")}
+                        />
                     </div>
                 </Form.Group>
-                {/* Form group for amount field */}
                 <Form.Group className="mt-4">
                     <Form.Label>Amount:</Form.Label>
-                    {/*... Form.Control code ...*/}
+                    <Form.Control
+                        type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        required
+                    />
                 </Form.Group>
-                {/* Form group for location field */}
                 <Form.Group className="mt-4">
                     <Form.Label>Location:</Form.Label>
-                    {/*... Form.Control code ...*/}
+                    <Form.Control
+                        type="text"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        required
+                    />
                 </Form.Group>
-                {/* Form group for date field */}
                 <Form.Group className="mt-4">
                     <Form.Label>Date:</Form.Label>
-                    {/*... Form.Control code ...*/}
+                    <Form.Control
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        required
+                    />
                 </Form.Group>
                 <div className="d-flex justify-content-between">
                     <Button variant="primary" type="submit" className="my-5">
